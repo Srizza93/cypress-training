3af5d733b3a8dc0aa7fa10353849682031b3b00c
Simone Rizza <simonerizza@MacBook-Pro-de-Simone.local>
2023-11-09 15:32:11 +0100

    testing library

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -3,6 +3,8 @@
 describe('My First Test', () => {
   it('visits the app root url', () => {
     cy.visit('/')
+    cy.get("[data-test-id='toto']")
+    cy.findByRole('link', { name: 'about' }).click()
     cy.contains('h1', 'You did it!')
   })
 })
