3908d8342009863fbe6396b6f3e578cfa01f0c80
Simone Rizza <simonerizza@MacBook-Pro-de-Simone.local>
2023-11-09 15:50:54 +0100

    attrinute on html div

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main>
-    <TheWelcome data-test-id="toto" />
+    <div data-test-id="toto"></div>
+    <TheWelcome />
   </main>
 </template>
